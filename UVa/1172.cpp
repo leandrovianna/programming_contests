20cb59ba5ad09b9ce8db58ef07fe146f4769b951
leandrovianna <leandro97vianna@gmail.com>
2019-05-02 01:26:15 -0300

    AC UVa 1172

--- UVa/1172.cpp
+++ UVa/1172.cpp
@@ -2,17 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt = 0;
-map<string, int> idx;
-
 const int N = 1010;
-int type1[N], type2[N];
+string type1[N], type2[N];
 int64_t v1[N], v2[N];
-
-bool operator<(pair<int64_t, int> a, pair<int64_t, int> b) {
-    return a.first < b.first
-        || (a.first == b.first && a.second >= b.second);
-}
 
 pair<int64_t, int> operator+(pair<int64_t, int> a, pair<int64_t, int> b) {
     return {a.first + b.first, a.second + b.second};
@@ -22,6 +14,18 @@ pair<int64_t, int> dp[N][N];
 int used[N][N], vis = 1;
 
 int n, m;
+
+pair<int64_t, int> get_best(pair<int64_t, int> p1, pair<int64_t, int> p2) {
+    if (p1.first < p2.first) {
+        return p2;
+    } else if (p1.first > p2.first) {
+        return p1;
+    } else if (p1.second < p2.second) {
+        return p1;
+    } else {
+        return p2;
+    }
+}
 
 pair<int64_t, int> solve(int i, int j) {
     if (i == n || j == m)
@@ -33,10 +37,11 @@ pair<int64_t, int> solve(int i, int j) {
     }
     used[i][j] = vis;
 
-    ans = max(solve(i+1, j), solve(i, j+1));
+    auto ans1 = solve(i+1, j), ans2 = solve(i, j+1);
+    ans = get_best(ans1, ans2);
 
     if (type1[i] == type2[j]) {
-        ans = max(ans, pair<int64_t, int>{v1[i] + v2[j], 1} + solve(i+1, j+1));
+        ans = get_best(ans, pair<int64_t, int>{v1[i] + v2[j], 1} + solve(i+1, j+1));
     }
 
     return ans;
@@ -48,30 +53,16 @@ int main() {
     cin >> t;
 
     while (t--) {
-        idx.clear();
-        cnt = 0;
-        int x;
-        string str;
-
         cin >> n;
+        string none;
         for (int i = 0; i < n; i++) {
-            cin >> str >> str >> x;
-            if (idx.count(str) == 0)
-                idx[str] = cnt++;
-
-            type1[i] = idx[str];
-            v1[i] = x;
+            cin >> none >> type1[i] >> v1[i];
         }
 
         cin >> m;
 
         for (int i = 0; i < m; i++) {
-            cin >> str >> str >> x;
-            if (idx.count(str) == 0)
-                idx[str] = cnt++;
-
-            type2[i] = idx[str];
-            v2[i] = x;
+            cin >> none >> type2[i] >> v2[i];
         }
 
         vis++;
