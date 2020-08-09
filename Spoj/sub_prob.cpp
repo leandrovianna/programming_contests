f2cf8e81e9ea66b92ac1e1424156c20460f59867
Leandro Vianna <leandro97vianna@gmail.com>
2020-08-09 03:03:45 -0300

    spoj

--- Spoj/sub_prob.cpp
+++ Spoj/sub_prob.cpp
@@ -1,131 +1,199 @@
 // Spoj - Substring Problem - SUB_PROB - TLE
 #include <bits/stdc++.h>
 using namespace std;
+const int M = 100100; // length of string
+const int N = 2*M, E = 26+26+10+5;
 
-const int N = 100100;
+struct SuffixAutomaton {
+  int edges[N][E];
+  int link[N];
+  int length[N];
+  bool term[N];
+  int last, sz;
+  int states;
 
-struct DoubleHash {
-    uint64_t h[2][N], poww[2][N];
-    uint64_t p[2], mod[2];
+  int code(char ch) {
+    if ('0' <= ch && ch <= '9')
+      return ch - '0';
 
-    DoubleHash() : DoubleHash(75, 1000000007, 232, 1000000009) {}
+    if ('a' <= ch && ch <= 'z')
+      return ch - 'a' + 10;
 
-    DoubleHash(uint64_t p1, uint64_t mod1, uint64_t p2, uint64_t mod2) {
-        p[0] = p1;
-        p[1] = p2;
-        mod[0] = mod1;
-        mod[1] = mod2;
+    if ('A' <= ch && ch <= 'Z')
+      return ch - 'A' + 10 + 26;
 
-        for (int j = 0; j < 2; j++) {
-            poww[j][0] = 1;
-            for (int i = 1; i < N; i++) {
-                poww[j][i] = (poww[j][i-1] * p[j]) % mod[j];
-            }
-        }
+    throw runtime_error("Character not supported");
+  }
 
+  void init() {
+    memset(edges, -1, sizeof(edges));
+    // add the initial node
+    states = 1;
+    link[0] = -1;
+    length[0] = 0;
+    term[0] = false;
+    last = 0; // initiate with index of first state
+    sz = 0; // length of longest suffix added to automaton
+  }
+
+  void from(string s) {
+    init();
+    for (const auto &ch : s) {
+      extend(ch);
+    }
+    find_terminals();
+  }
+
+  void extend(char ch) {
+    // create new state for new equivalence (end-points) class
+    sz++;
+    length[states] = sz;
+    link[states] = 0;
+    term[states] = false;
+    states++;
+
+    int r = states - 1;
+    int p = last;
+
+    while (p >= 0 && edges[p][code(ch)] == -1) {
+      edges[p][code(ch)] = r;
+      p = link[p];
     }
 
-    void init(const string &s) {
-        for (int idx = 0; idx < 2; idx++) {
-            h[idx][0] = 5381ULL;
-            for (int i = 0, n = s.size(); i < n; i++) {
-                int code = s[i];
-                h[idx][i+1] = (h[idx][i] * p[idx] + code) % mod[idx];
-            }
+    if (p != -1) {
+      int q = edges[p][code(ch)];
+      if (length[p] + 1 == length[q]) {
+        link[r] = q;
+      } else {
+        for (int e = 0; e < E; e++) {
+          edges[states][e] = edges[q][e];
         }
+        length[states] = length[p] + 1;
+        link[states] = link[q];
+        term[states] = false;
+        states++;
+
+        int qq = states - 1;
+        link[q] = qq;
+        link[r] = qq;
+
+        while (p >= 0 && edges[p][code(ch)] == q) {
+          edges[p][code(ch)] = qq;
+          p = link[p];
+        }
+      }
     }
 
-    pair<uint64_t, uint64_t> get_hash(int i, int j) {
-        uint64_t r[2];
-        for (int idx = 0; idx < 2; idx++) {
-            r[idx] = (h[idx][j+1] + mod[idx]
-                - (h[idx][i] * poww[idx][j-i+1]) % mod[idx]) % mod[idx];
-        }
-        return {r[0], r[1]};
+    last = r;
+  }
+
+  void find_terminals() {
+    memset(term, 0, sizeof(term));
+    int p = last;
+    while (p >= 0) {
+      term[p] = true;
+      p = link[p];
     }
+  }
 };
 
-DoubleHash h1, h2;
+#ifdef __WIN32__
+#define getchar_unlocked getchar
+#define putchar_unlocked putchar
+#endif
 
 bool readChar(char &c) {
-	c = getchar_unlocked();
-	return c != EOF;
+  c = getchar_unlocked();
+  return c != EOF;
 }
 
 inline void writeChar(char c) {
-	putchar_unlocked(c);
+  putchar_unlocked(c);
 }
 
 template<typename T>
 bool readInt( T &n ) { 
-    n = 0; 
-    register bool neg = false; 
-    register char c = getchar_unlocked(); 
-    if( c == EOF) { n = -1; return false; } 
-    while (!('0' <= c && c <= '9')) { 
-        if( c == '-' ) neg = true; 
-        c = getchar_unlocked(); 
-    } 
-    while ('0' <= c && c <= '9') { 
-        n = n * 10 + c - '0'; 
-        c = getchar_unlocked(); 
-    } 
-    n = (neg ? (-n) : (n)); 
-    return true; 
+  n = 0; 
+  bool neg = false; 
+  char c = getchar_unlocked(); 
+  if( c == EOF) { n = -1; return false; } 
+  while (!('0' <= c && c <= '9')) { 
+    if( c == '-' ) neg = true; 
+    c = getchar_unlocked(); 
+  } 
+  while ('0' <= c && c <= '9') { 
+    n = n * 10 + c - '0'; 
+    c = getchar_unlocked(); 
+  } 
+  n = (neg ? (-n) : (n)); 
+  return true; 
 } 
- 
+
 template<typename T>
 inline void writeInt(T n){ 
-    register int idx = 20; 
-    if( n < 0 ) putchar_unlocked('-'); 
-    n = abs(n); 
-    char out[21]; 
-    out[20] = ' '; 
-    do{ 
-        idx--; 
-        out[idx] = n % 10 + '0'; 
-        n/= 10; 
-    }while(n); 
-    do{ putchar_unlocked(out[idx++]); } while (out[idx] != ' '); 
+  int idx = 20; 
+  if( n < 0 ) putchar_unlocked('-'); 
+  n = abs(n); 
+  char out[21]; 
+  out[20] = ' '; 
+  do{ 
+    idx--; 
+    out[idx] = n % 10 + '0'; 
+    n/= 10; 
+  }while(n); 
+  do{ putchar_unlocked(out[idx++]); } while (out[idx] != ' '); 
 } 
 
-void readString(string &s) {
-    char ch = 'l';
-    s = "";
-    while (ch != '\n') {
-        readChar(ch);
-        s += ch;
-    }
-    s.pop_back();
+string readString() {
+  char ch;
+  string s = "";
+  // ignore white-spaces
+  while (readChar(ch) && isspace(ch)) continue;
+  if (ch == EOF) return s;
+  do {
+    s += ch;
+  } while (readChar(ch) && !isspace(ch));
+  return s;
 }
 
+void writeString(const string &s) {
+  for (const auto &ch : s) {
+    writeChar(ch);
+  }
+}
+
+SuffixAutomaton sa;
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int n;
-    string s = "", p;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
-    readString(s);
-    int m = s.size();
+  int n;
+  string s, p;
 
-    h1.init(s);
+  s = readString();
+  sa.from(s);
 
-    readInt(n);
+  readInt(n);
 
-    for (int ni = 0; ni < n; ni++) {
-        readString(p);
-        h2.init(p);
-        char ans = 'N';
-        for (int i = 0, l = p.size(); i <= m - l; i++) {
-            if (h1.get_hash(i, i+l-1) == h2.get_hash(0, l-1)) {
-                ans = 'Y';
-                break;
-            }
-        }
-        writeChar(ans);
-        writeChar('\n');
+  for (int ni = 0; ni < n; ni++) {
+    p = readString();
+    bool isSubstring = true;
+
+    int st = 0, nxt;
+    for (const auto &ch : p) {
+      nxt = sa.edges[st][sa.code(ch)];
+      if (nxt == -1) {
+        isSubstring = false;
+        break;
+      }
+
+      st = nxt;
     }
 
-    return 0;
+    writeString(isSubstring ? "Y\n" : "N\n");
+  }
+
+  return 0;
 }
--- lib/getchar_unlocked.cpp
+++ lib/getchar_unlocked.cpp
@@ -7,46 +7,64 @@ using namespace std;
 #endif
 
 bool readChar(char &c) {
-	c = getchar_unlocked();
-	return c != EOF;
+  c = getchar_unlocked();
+  return c != EOF;
 }
 
 inline void writeChar(char c) {
-	putchar_unlocked(c);
+  putchar_unlocked(c);
 }
 
 template<typename T>
-bool readInt( T &n ) { 
-    n = 0; 
-    register bool neg = false; 
-    register char c = getchar_unlocked(); 
-    if( c == EOF) { n = -1; return false; } 
-    while (!('0' <= c && c <= '9')) { 
-        if( c == '-' ) neg = true; 
-        c = getchar_unlocked(); 
-    } 
-    while ('0' <= c && c <= '9') { 
-        n = n * 10 + c - '0'; 
-        c = getchar_unlocked(); 
-    } 
-    n = (neg ? (-n) : (n)); 
-    return true; 
-} 
- 
+bool readInt( T &n ) {
+  n = 0;
+  bool neg = false;
+  char c = getchar_unlocked();
+  if( c == EOF) { n = -1; return false; }
+  while (!('0' <= c && c <= '9')) {
+    if( c == '-' ) neg = true;
+    c = getchar_unlocked();
+  }
+  while ('0' <= c && c <= '9') {
+    n = n * 10 + c - '0';
+    c = getchar_unlocked();
+  }
+  n = (neg ? (-n) : (n));
+  return true;
+}
+
 template<typename T>
-inline void writeInt(T n){ 
-    register int idx = 20; 
-    if( n < 0 ) putchar_unlocked('-'); 
-    n = abs(n); 
-    char out[21]; 
-    out[20] = ' '; 
-    do{ 
-        idx--; 
-        out[idx] = n % 10 + '0'; 
-        n/= 10; 
-    }while(n); 
-    do{ putchar_unlocked(out[idx++]); } while (out[idx] != ' '); 
-} 
+inline void writeInt(T n){
+  int idx = 20;
+  if( n < 0 ) putchar_unlocked('-');
+  n = abs(n);
+  char out[21];
+  out[20] = ' ';
+  do{
+    idx--;
+    out[idx] = n % 10 + '0';
+    n/= 10;
+  }while(n);
+  do{ putchar_unlocked(out[idx++]); } while (out[idx] != ' ');
+}
+
+string readString() {
+  char ch;
+  string s = "";
+  // ignore white-spaces
+  while (readChar(ch) && isspace(ch)) continue;
+  if (ch == EOF) return s;
+  do {
+    s += ch;
+  } while (readChar(ch) && !isspace(ch));
+  return s;
+}
+
+void writeString(const string &s) {
+  for (const auto &ch : s) {
+    writeChar(ch);
+  }
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -55,6 +73,10 @@ int main() {
 	while (readInt(x)) {
 		writeInt(x);
 	}
+
+	string s = readString();
+	writeString(s);
+	writeChar('\n');
 	return 0;
 }
 
